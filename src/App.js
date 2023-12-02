5aca3ac873d6f3522b0ce33dc89c61a3ce55e69c
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-12-02 09:24:55 +0530

    simple client side link to mongodb and data insert

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/users`)
+      .then(res => res.json())
+      .then(data => {
+        setUser(data)
+      })
+  }, [])
+
+const handleAddUser = event =>{
+  event.preventDefault();
+  const form = event.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  console.log(name, email);
+  const user = {name, email}
+
+  fetch(`http://localhost:5000/users`,{
+    method:'POST',
+    headers:{
+      'Content-type':'application/json'
+    },
+    body:JSON.stringify(user)
+  })
+  .then(res => res.json())
+  .then(data => {
+    const newUser = [...user, data];
+    setUser(newUser)
+  })
+  .catch(err => console.log(err))
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<form onSubmit={handleAddUser}>
+  <input type='text' placeholder='enter your name' name='name' /><br/>
+  <input type='email' name='email'  placeholder='enter your email' /><br/>
+  <button>Add User</button>
+</form>
+
+
+
+      <h1>User: {user?.length} </h1>
+      {
+
+        user.map(u => <p key={u._id}>{u?.email}</p>)
+      }
     </div>
   );
 }
